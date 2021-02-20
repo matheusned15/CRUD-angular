c322791e431c2518ff0851ceff7f4ab81ccbb3b7
matheusned15 <matheusned15@gmail.com>
2021-02-20 01:04:54 -0300

    Componente excluir produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
@@ -21,6 +22,10 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
   {
     path: "products/update/:id",
     component: ProductUpdateComponent
+  },
+  {
+    path: "products/delete/:id",
+    component: ProductDeleteComponent
   },
   ];
 
